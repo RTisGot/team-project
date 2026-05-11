e3f35acd10b4985fc9a369e313fca3d4a95745c1
student <mit2571548@stu.o-hara.ac.jp>
2026-05-11 16:23:06 +0900

    1

---
+++ ソース.cpp
@@ -0,0 +1,28 @@
+﻿#include "DxLib.h"
+
+int WINAPI WinMain(
+    HINSTANCE,
+    HINSTANCE,
+    LPSTR,
+    int)
+{
+    ChangeWindowMode(TRUE);
+
+    if (DxLib_Init() == -1)
+    {
+        return -1;
+    }
+
+    while (ProcessMessage() == 0)
+    {
+        ClearDrawScreen();
+
+        DrawString(100, 100, "Hello DxLib!", GetColor(255, 255, 255));
+
+        ScreenFlip();
+    }
+
+    DxLib_End();
+
+    return 0;
+}
