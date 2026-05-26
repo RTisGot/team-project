d39dc3a44641a6224594cffca36766a4997c6236
student <mit2571548@stu.o-hara.ac.jp>
2026-05-26 12:39:48 +0900

    ExploreSceneとGameDataの仮設定

---
+++ Game/source/scene/ExploreScene.cpp
@@ -0,0 +1,21 @@
+#include "scene/ExploreScene.h"
+
+ExploreScene::ExploreScene(SceneManager* manager)
+    : m_manager(manager)
+{
+}
+
+void ExploreScene::Init()
+{
+    // 探索シーンの初期化処理
+}
+
+void ExploreScene::Update()
+{
+    // 探索シーンの更新処理
+}
+
+void ExploreScene::Draw()
+{
+    // 探索シーンの描画処理
+}
