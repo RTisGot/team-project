be10c9b3dbb966fa79f5c48d836c090e9b0d1891
student <mit2571548@stu.o-hara.ac.jp>
2026-06-04 09:04:30 +0900

    コメント追加

--- Game/source/system/GameTimer.cpp
+++ Game/source/system/GameTimer.cpp
@@ -8,10 +8,13 @@ void GameTimer::Init()
 
 void GameTimer::Update()
 {
+    // 現在の時間をミリ秒単位で取得
     const int currentTime = GetNowCount();
 
+    // 前回からの経過時間を秒単位で計算
     m_DeltaTime =static_cast<float>(currentTime - m_PreviousTime)/ 1000.0f;
 
+    // フレームレートを安定させるために、デルタタイムが極端に大きくならないように制限
     m_PreviousTime = currentTime;
 }
 
