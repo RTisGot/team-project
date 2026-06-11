a8c3136d05fe2148e693828ff52f1e7df8c1a149
村田 智仁 <mit2571561@stu.o-hara.ac.jp>
2026-06-11 11:11:16 +0900

    お供のスキル追加

--- Game/source/scene/ExploreScene.cpp
+++ Game/source/scene/ExploreScene.cpp
@@ -57,6 +57,8 @@ void ExploreScene::Init()
     m_follower = std::make_unique<Follower>();
     m_follower->LoadModel();
 
+    m_follower->SetOrbManager(m_OrbManager.get());
+
     // 当たり判定マネージャーの生成とステージモデル登録
     m_collisionManager = std::make_unique<CollisionManager>();
     m_collisionManager->Init(m_CurrentMap->GetModelHandle());
